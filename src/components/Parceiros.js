df7b43af8349a9b3d416464aca9fba4eaf66543a
LuigiHard <luigigandolfi@hotmail.com>
2024-03-13 15:29:28 -0300

    a

--- src/components/Parceiros.js
+++ src/components/Parceiros.js
@@ -1,43 +1,72 @@
 import React from 'react';
-import { Component } from 'react';
+import { CForm, CCol, CFormInput, CFormSelect, CFormCheck, CButton } from '@coreui/react';
 import styled from 'styled-components';
-
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  width: 100%;
   
   background-position: center;
   background-size: cover;
   height: 100vh;
 `;
-const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 20px 80px;
-`;
-const ScrollContainer = styled.div`
-  width: 60%;
-  height: 80%; // Adjust based on your needs
-  overflow-y: auto; // Enables vertical scrolling
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-  padding: 20px; // Adds some padding inside the scroll container
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1); // Optional: adds a shadow for better depth perception
+const StyledCForm = styled(CForm)`
+width: 80%;
+margin: 0 auto;
+
 `;
 
 const Parceiros= () => {
+  const { isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  }
     return (
       <Container>
-        <ContentWrapper>
-          <ScrollContainer>
-            <h1>Parceiros</h1>
-            <p>Conheça nossos parceiros</p>
-          </ScrollContainer>
-        </ContentWrapper>
+        <StyledCForm  className="row g-3">
+          <CCol md={4}>
+            <CFormInput type="name" id="inputEmail4" label="Nome" />
+          </CCol>
+          <CCol md={6}>
+            <CFormInput type="surname" id="inputEmail4" label="Sobrenome" />
+          </CCol>
+          <CCol md={6}>
+            <CFormInput type="email" id="inputEmail4" label="Email Corporativo" />
+          </CCol>
+          
+          <CCol xs={12}>
+            <CFormInput id="inputAddress" label="" placeholder="1234 Main St"/>
+          </CCol>
+          <CCol xs={12}>
+            <CFormInput id="inputAddress2" label="Address 2" placeholder="Apartment, studio, or floor"/>
+          </CCol>
+          <CCol md={6}>
+            <CFormInput id="inputCity" label="City"/>
+          </CCol>
+          <CCol md={4}>
+            <CFormSelect id="inputState" label="State">
+              <option>Choose...</option>
+              <option>...</option>
+            </CFormSelect>
+          </CCol>
+          <CCol md={2}>
+            <CFormInput id="inputZip" label="Zip" />
+          </CCol>
+          <CCol xs={12}>
+            <CFormCheck type="checkbox" id="gridCheck" label="Check me out"/>
+          </CCol>
+          <CCol xs={12}>
+            <CButton color="primary" type="submit">Sign in</CButton>
+          </CCol>
+        </StyledCForm>
       </Container>
     );
   }
