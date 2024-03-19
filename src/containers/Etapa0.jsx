20075805d3cc6050d8550395681e37a94cd22c73
LuigiHard <79230985+LuigiHard@users.noreply.github.com>
2024-03-19 12:50:56 -0300

    kolhjvbiyugtftyivf

--- src/containers/Etapa0.jsx
+++ src/containers/Etapa0.jsx
@@ -36,7 +36,8 @@ const Paragraph = styled.p`
 
 const RoleButton = styled.button`
   margin: 10px;
-  padding: 10px 20px;
+  padding: 10px 00px;
+  width: 200px;
   background-color: ${(props) => (props.isSelected ? '#2ebc15' : 'transparent')};
   color: ${(props) => (props.isSelected ? 'white' : '#2ebc15')};
   border: 1px solid #2ebc15;
